d83a3bee20df1e7e86c78f04bbd14553b9be4195
Fahad Momin <ffmomin43@gmail.com>
2023-08-20 16:19:48 +0530

    Initial Commit

---
+++ ShopNow.Repository/EF/ECommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNow.Repository.EF
+{
+    internal class ECommerceContext: DbContext
+    {
+        public ECommerceContext():base("ECommConnSt")
+        {
+                
+        }
+
+
+    }
+}
