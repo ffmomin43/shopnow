f4f6837484bd426f8cb76ad0d04678af7a1c2d48
Fahad Momin <ffmomin43@gmail.com>
2023-09-10 14:37:35 +0530

    Added Test Controller Part 1

---
+++ ShopNow.Repository/Common/Repository/Impl/CategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShopNow.Model;
+using ShopNow.Repository.Common.Repository.Intefrace;
+using ShopNow.Repository.EF;
+
+namespace ShopNow.Repository.Common.Repository.Impl
+{
+    public class CategoryRepository : EFRepositoryBase<ECommerceContext, Category, int>, ICategoryRepository
+    {
+    }
+}
---
+++ ShopNow.Repository/Common/Repository/Intefrace/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopNow.Model;
+using ShopNow.Repository.Common.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNow.Repository.Common.Repository.Interface
+{
+    public interface IProductRepository: IRepository<Product, int>
+    {
+
+    }
+}
